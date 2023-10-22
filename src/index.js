a2406a34ab965f745e8abee0dc1d07f4f67bea10
DianaSmertina <am.dianasmertina@gmail.com>
2023-10-22 11:54:45 +0800

    add modal menu for tab and mobile

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ var slider = tns({
         },
         501: {
             items: 2,
-        }
+        },
     },
 });
 
@@ -124,3 +124,25 @@ window.onload = function () {
     changeHeight(reviewsColumns);
     changeHeight(stagesColumns);
 };
+
+const burgerBtn = document.querySelector(".header__burger");
+const closeMenu = document.querySelector(".menu-modal__close");
+const modal = document.querySelector(".menu-modal");
+const body = document.body;
+const modalLink = Array.from(document.querySelectorAll(".menu-modal__nav-link"));
+
+burgerBtn.addEventListener("click", () => {
+    modal.classList.remove("none");
+    body.classList.add("no-scroll");
+});
+
+function closeModal() {
+    modal.classList.add("none");
+    body.classList.remove("no-scroll");
+}
+
+closeMenu.addEventListener("click", closeModal);
+
+modalLink.forEach((link) => {
+    link.addEventListener("click", closeModal);
+});
